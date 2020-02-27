a110c270235703cd75b9a102933ac5d40b982e11
Iqbal Hamdani <iqbal.hamdani@misteraladin.com>
2020-02-27 15:06:48 +0700

    change name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const postcssPresetEnv = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  title: 'V-Calendar',
+  title: 'V-Calendar-Custom',
   description: 'An elegant calendar and datepicker plugin for Vuejs.',
   plugins: [
     // Google analytics
@@ -105,7 +105,7 @@ module.exports = {
       ],
     },
     // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'nathanreyes/v-calendar',
+    repo: 'miqbalhamdani/v-calendar-custom',
     // if your docs are not at the root of the repo
     docsDir: 'docs',
     // optional, defaults to master
